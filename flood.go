f2c45f5cd18fae5a91a13239fe38f1e930f1cf9c
NgThanhVinh <117195555+Vntvxyz@users.noreply.github.com>
2023-02-04 16:57:17 +0700

    Update flood.go

--- flood.go
+++ flood.go
@@ -32,15 +32,15 @@ func main(){
   target.url = os.Args[1]
   target.method = os.Args[2]
   threads, _ := strconv.Atoi(os.Args[3])
-  Println(Fore.RED +"Đang Khởi Động                           ")
-  Println(Fore.RED +" [..              [..                    ") 
-  Println(Fore.RED +" [..              [..                    ")
-  Println(Fore.RED +" [..[.. [..       [..[. [...   [..       ") 
-  Println(Fore.RED +" [.. [..  [.. [.. [.. [..    [..  [..    ") 
-  Println(Fore.RED +" [.. [..  [..[.   [.. [..   [..   [..    ")
-  Println(Fore.RED +" [.. [..  [..[.   [.. [..   [..   [..    ") 
-  Println(Fore.RED +" [..[...  [.. [.. [..[...     [.. [...   ")
-  Println(Fore.RED +"              Tấn Công DDoS Flood By NTV ")
+  Println("Đang Khởi Động")
+  Println(" [..              [..") 
+  Println(" [..              [.. ")
+  Println(" [..[.. [..       [..[. [...   [.. ") 
+  Println(" [.. [..  [.. [.. [.. [..    [..  [..") 
+  Println(" [.. [..  [..[.   [.. [..   [..   [..")
+  Println(" [.. [..  [..[.   [.. [..   [..   [..") 
+  Println(" [..[...  [.. [.. [..[...     [.. [...")
+  Println("              Tấn Công DDoS Flood By NTV ")
    for i:=0; i < threads; i++{
      go httpflood()
    }
